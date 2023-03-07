f9fb92eb5b50ae4b08ba6d3e8cda3b7ae0456276
Namanpreet Ghotra <ng00451@surrey.ac.uk>
2023-03-07 18:26:29 +0000

    First commit

---
+++ src/wordCount/count.java
@@ -0,0 +1,34 @@
+package wordCount;
+import java.util.Scanner;
+
+public class count {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Insert your text:");
+		Scanner scan = new Scanner(System.in);
+		
+		String userInput = scan.nextLine();
+		scan.close();
+		userInput = userInput.trim();
+		
+		int count = 0;
+		
+		if(userInput.length()==0) {
+			count =0;
+			
+		}
+		else {
+			count ++;
+			for(int i =0; i < userInput.length(); i++) {
+				if(userInput.charAt(i) == ' ' && userInput.charAt(i+1) != ' ') {
+					count++;
+				}
+			}
+		}
+		
+		System.out.println("Word Count: " +count);
+		
+		
+	}
+}
